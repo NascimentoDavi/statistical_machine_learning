6228cd78fa0c2791e3f140181eb0e7d65d1343a7
Davi Rodrigues <davirnascimentox@gmail.com>
2025-05-16 21:32:39 -0300

    feat: knn | label encoder | matriz de confusao

--- knn_lbencoder.py
+++ knn_lbencoder.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 DATASET = './datasets/loan3000.csv'
 
@@ -13,11 +15,12 @@ df = pd.read_csv(DATASET)
 df = df.drop(columns=['Unnamed: 0'])
 
 # Instanciar Label Encoder
-label_encoder = LabelEncoder()
+label_encod_outcome = LabelEncoder()
+label_encod_purpose = LabelEncoder()
 
 # Codificar colunas categóricas utilizando LabelEncoder
-df['outcome'] = label_encoder.fit_transform(df['outcome']) #target
-df['purpose_'] = label_encoder.fit_transform(df['purpose_']) # feature categórica
+df['outcome'] = label_encod_outcome.fit_transform(df['outcome']) #target
+df['purpose_'] = label_encod_purpose.fit_transform(df['purpose_']) # feature categórica
 
 # target = variável que modelo deve prever
 # outcome = variável utilizada para tentar realizar as previsões
@@ -42,4 +45,16 @@ y_pred = knn.predict(x_test)
 
 # Avaliar acurácia
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Acurácia do KNN (k=5): {accuracy:.2f}")
+# print(f"Acurácia do KNN (k=5): {accuracy:.2f}")
+
+# Gerar matriz de confusão
+# Na matriz de confusã comparamos o que o modelo previu com a verdade em si, por isso utilizamos y_test e y_pred
+
+cm = confusion_matrix(y_test, y_pred)
+
+# Mostrar matriz de confusão de forma BUNITA
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=label_encod_outcome.classes_)
+
+disp.plot(cmap=plt.cm.Blues)
+plt.title("Matriz de Confusão - KNN")
+plt.show()
